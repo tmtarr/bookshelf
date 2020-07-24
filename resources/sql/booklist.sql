e1b5859b156ec3bf24485ba69fda2f508f62fdbd
testU <test@example.com>
2020-07-24 18:15:31 +0900

    項目の並び順整理およびrate追加

--- resources/sql/booklist.sql
+++ resources/sql/booklist.sql
@@ -1,19 +1,20 @@
 -- create table
 create table booklist(
 	id varchar(5),
+	userid varchar(20),
+	isbn13 varchar(13),
 	bookname varchar(200),
 	author varchar(100),
+	publisher varchar(100),
 	category varchar(100),
-	place varchar(20),
+	read_date varchar(10),
 	note varchar(100),
-	isbn13 varchar(13),
-	ebook_flg varchar(1),
-	wish_flg varchar(1),
-	userid varchar(20),
 	ins_date varchar(19),
 	upd_date varchar(19),
-	publisher varchar(100),
-	read_date varchar(10)
+	ebook_flg varchar(1),
+	wish_flg varchar(1),
+	place varchar(20),
+	rate varchar(2)
 );
 
 -- primary key
