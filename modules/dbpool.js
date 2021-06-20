b673f85b51c91164c9c01bf01fb228ea4d4d01f7
fuyu <ddit01+tmtarr@gmail.com>
2021-06-20 19:23:26 +0900

    herokuのSSLエラー対応

--- modules/dbpool.js
+++ modules/dbpool.js
@@ -1,6 +1,15 @@
 // DB接続
 const { Pool } = require('pg');
-const pool = new Pool({
+const config = {
 	connectionString: process.env.DATABASE_URL
-});
+}
+if (process.env.SSL != 'false') {
+	config.ssl = {
+		rejectUnauthorized: false
+	}
+}
+const pool = new Pool(config);
+console.log('接続ログ↓');
+console.log(pool);
+console.log('接続ログ↑');
 module.exports = pool;
